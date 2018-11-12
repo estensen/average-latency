f0996fe6538cc10285be32f1c7b72689afe640d1
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-12 19:23:41 +0100

    Get response time

--- average-latency.go
+++ average-latency.go
@@ -1,13 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"time"
+)
 
 func querySite(site string) {
 	fmt.Println("Querying ", site)
+	conn, err := net.Dial("tcp", site)
+	defer conn.Close()
+
+	conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+
+	start := time.Now()
+	oneByte := make([]byte, 1)
+	_, err = conn.Read(oneByte)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("First byte:", time.Since(start))
+
+	_, err = ioutil.ReadAll(conn)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	sites := []string{"vg.no", "nrk.no", "adressa.no"}
+	sites := []string{"vg.no:80", "nrk.no:80", "aftenposten.no:80"}
 
 	for _, site := range sites {
 		querySite(site)
