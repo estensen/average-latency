93083138092b58f578658d23deffb492c0612d37
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-13 11:21:31 +0100

    Create avg latency flag

--- ping.go
+++ ping.go
@@ -9,9 +9,14 @@ import (
 	"time"
 )
 
-func querySites(urls []string) {
+func querySites(urls []string, avgFlag bool) {
 	for _, url := range urls {
-		querySite(url)
+		switch avgFlag {
+		case true:
+			getLatency(url)
+		default:
+			querySite(url)
+		}
 	}
 }
 
@@ -37,13 +42,46 @@ func querySite(url string) {
 	fmt.Println("All bytes:", time.Since(start))
 }
 
+func getLatency(url string) {
+	fmt.Println("Querying ", url)
+	conn, err := net.Dial("tcp", url+":80")
+	defer conn.Close()
+
+	conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+
+	start := time.Now()
+	oneByte := make([]byte, 1)
+	_, err = conn.Read(oneByte)
+	if err != nil {
+		panic(err)
+	}
+
+	latency := time.Since(start)
+	fmt.Println("First byte:", latency)
+
+	_, err = ioutil.ReadAll(conn)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	app := &cli.App{
 		Name: "ping",
 		Usage: "get latencies of web sites",
+
+		Flags: []cli.Flag {
+			&cli.BoolFlag {
+				Name: "average",
+				Aliases: []string{"a"},
+				Usage: "Display average latency",
+			},
+		},
 		Action: func(c *cli.Context) error {
 			urls := c.Args().Slice()
-			querySites(urls)
+			avgFlag := c.Bool("average")
+
+			querySites(urls, avgFlag)
 			return nil
 		},
 	}
